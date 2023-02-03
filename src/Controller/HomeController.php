54e490b20a73db856cd24fa5b9cbb1e6934e6bcf
Theo Journee <theo_journee@ens.univ-artois.fr>
2023-02-03 19:56:38 +0100

    Fichier final.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,15 +6,27 @@ use Symfony\Component\HttpFoundation\Request;
 
 class HomeController
 {
-
     /**
      * Méthode à gérer un fichier.
      * @param $reform
      * @param Request $request
      * @return void
+     * @Route("/file/{reform}", name="develop_file")
      */
     public function DevelopFile($reform,Request$request ){
 
-    }
+        $file = $request->query->get('file');
+        $fileContent = file_get_contents($file);
+        $fileContent = mb_convert_case($fileContent, MB_CASE_UPPER, "UTF-8");
+        if ($reform === 'lower') {
+            $newFile = mb_convert_case($fileContent, MB_CASE_LOWER, "UTF-8");
+        }
 
+        $newFile = 'New' . $file;
+        file_put_contents($newFile, $fileContent);
+
+
+        return $this->md(['file' => $newFile]);
+
+    }
 }
