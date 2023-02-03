a8de3b43175ad9acaa701646d5834561aa6e8549
Theo Journee <theo_journee@ens.univ-artois.fr>
2023-02-03 19:04:44 +0100

    Ajout de la documentation.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,6 +7,12 @@ use Symfony\Component\HttpFoundation\Request;
 class HomeController
 {
 
+    /**
+     * Méthode qui sert à créer un fichier et le corriger.
+     * @param $reform 
+     * @param Request $request
+     * @return void
+     */
     public function DevelopFile($reform,Request$request ){
 
     }
